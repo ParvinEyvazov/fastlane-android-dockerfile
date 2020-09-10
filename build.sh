57916ed5a2dc5ea77991fb79fba91fe4225a9fbb
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-09-10 20:05:43 +0300

    curl function updated

--- build.sh
+++ build.sh
@@ -19,10 +19,26 @@ chmod +x gradlew
 curl -X POST https://tdgames-8cd80.hq.spicaengine.com/api/fn-execute/storageMaster -H "Content-Type: application/json" --data {\"content\":\"$(echo 'start building' | base64)\"}
 
 
-fastlane build
+# fastlane build
 
 echo "SUCCESSFULLY BUILD!"
 
-curl -X POST https://tdgames-8cd80.hq.spicaengine.com/api/fn-execute/storageMaster -H "Content-Type: application/json" --data {\"content\":\"$(echo 'end of building' | base64)\"}
+echo "test test build" > deneme.apk
+
+
+
+# cd $REPOSITORY_NAME/app/build/outputs/apk/release/
+
+echo "[
+    {
+        \"name\": \"file.json\",
+        \"content\": {
+            \"type\": \"image/png\",
+            \"data\": \"$(cat deneme.apk|base64)\"
+        }
+    }
+]">file.json
+
+curl https://tdgames-8cd80.hq.spicaengine.com/api/fn-execute/storageMaster --verbose --header "Authorization: APIKEY 11w6w5s71keu1xuuj" -d "@file.json" --header "Content-Type:application/json"
 
 echo "SENDED POST REQUEST"
